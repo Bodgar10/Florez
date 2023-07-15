14d1b719e0853b5beb69cf955bb3472e92f47c07
Bodgar Miranda <Bodgar.Miranda@walmart.com>
2023-07-14 19:04:15 -0600

    Añadiendo la lógica del diseño del botón que se usará en toda la app.

--- Florez/ContentView.swift
+++ Florez/ContentView.swift
@@ -20,7 +20,10 @@ struct ContentView: View {
                 Image("bg")
                     .resizable()
                     .edgesIgnoringSafeArea(.all)
-                Text("Select an item")
+                HStack {
+                    ButtonFlorez(buttonStyle: .special, title: "Iniciar sesion")
+                    ButtonFlorez(buttonStyle: .principal, title: "REGISTRARSE")
+                }
             }
            
         }
@@ -30,6 +33,64 @@ struct ContentView: View {
 }
 
 
+struct ButtonFlorez: View {
+    
+    enum ButtonStyle {
+        case principal
+        case secondary
+        case special
+        case promotion
+        case promotion2
+        case promotion3
+        
+        var backgroundColor: Color {
+            switch self {
+            case .special:
+                    return .clear
+            case .secondary:
+                return .white
+            case .principal:
+                return .pink
+            case .promotion, .promotion2, .promotion3:
+                return .orange
+            }
+        }
+        
+        var textColor: Color {
+            switch self {
+            case .special:
+                    return .pink
+            case .secondary:
+                return .pink
+            case .principal:
+                return .white
+            case .promotion, .promotion2, .promotion3:
+                return .white
+            }
+        }
+        
+    }
+    
+    
+    var buttonStyle: ButtonStyle
+    var title: String
+    
+    init(buttonStyle: ButtonStyle, title: String) {
+        self.buttonStyle = buttonStyle
+        self.title = title
+    }
+    
+    var body: some View {
+        Button {
+            print("Click")
+        } label: {
+            Text(title.uppercased())
+                .foregroundColor(buttonStyle.textColor)
+        }
+        .background(buttonStyle.backgroundColor)
+        .cornerRadius(5)
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
